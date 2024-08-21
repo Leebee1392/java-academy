b1dcdab24cfaa34d717b45314464ae0f9487185f
Leanne Billington <leanne.billington@kainos.com>
2024-08-21 14:51:35 +0100

    add test config and fixed build

--- src/main/java/org/example/TestApplication.java
+++ src/main/java/org/example/TestApplication.java
@@ -6,8 +6,11 @@ import io.dropwizard.setup.Environment;
 import io.federecio.dropwizard.swagger.SwaggerBundle;
 import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 import org.example.controllers.RolesController;
+import org.example.controllers.TestController;
 import org.example.daos.RolesDao;
+import org.example.daos.TestDao;
 import org.example.services.RolesService;
+import org.example.services.TestService;
 
 public class TestApplication extends Application<TestConfiguration> {
     public static void main(final String[] args) throws Exception {
@@ -30,6 +33,8 @@ public class TestApplication extends Application<TestConfiguration> {
     @Override
     public void run(final TestConfiguration configuration,
                     final Environment environment) {
+        environment.jersey()
+                .register(new TestController(new TestService(new TestDao())));
         environment.jersey()
                 .register(new RolesController(
                         new RolesService((new RolesDao()))));
--- src/main/java/org/example/services/TestService.java
+++ src/main/java/org/example/services/TestService.java
@@ -1,5 +1,7 @@
 package org.example.services;
 
+import org.example.daos.TestDao;
+
 import java.sql.SQLException;
 import java.util.List;
 
